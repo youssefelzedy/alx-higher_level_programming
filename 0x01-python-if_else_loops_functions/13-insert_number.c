0de67ed577faba09ad9e228dafaa7b95ed585ec1
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-03 22:50:26 +0300

    0x00-python-hello_world

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -28,6 +28,7 @@ listint_t *insert_node(listint_t **head, int number)
 		while (current->next != NULL && current->next->n < number)
 			current = current->next;
 		new->next = current->next;
+		current->next = new;
 	}
 	return (new);
 }
