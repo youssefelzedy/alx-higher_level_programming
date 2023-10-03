d75afa9bde76200178581a39e0dabaae9319006f
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-03 22:09:13 +0300

    0x00-python-hello_world

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     digit = 10 - (number % 10) if number < 0 else number % 10
-    return (print("{}".format(digit), end=""))
+    print("{}".format(digit), end="")
+    return (digit)
