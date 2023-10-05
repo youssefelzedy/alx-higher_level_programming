dd9ffab7e4443b18f1378ab24968fd9867636376
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-05 21:25:58 +0300

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,12 @@
-import sys
-if len(sys.argv) == 1:
-    print("{} arguments.".format(len(sys.argv) - 1))
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    i = 0
-    for thing in sys.argv:
-        if i != 0:
-            print("{}: {}".format(i, thing))
-        i += 1
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        i = 0
+        for thing in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, thing))
+            i += 1
