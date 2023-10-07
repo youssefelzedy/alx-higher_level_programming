e9d299db0babf20fd1ad609c88151a41ac944b93
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-07 12:24:16 +0300

    0x02-python-import_modules

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,8 +3,10 @@ def new_in_list(my_list, idx, element):
     if my_list:
         arr = []
         if (idx < 0):
+            arr = my_list
             return (my_list)
         if (idx > len(my_list)):
+            arr = my_list
             return (my_list)
         for i in range(len(my_list)):
             if (i == idx):
