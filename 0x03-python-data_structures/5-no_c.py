21133189034120e8f371100489e2a87e5d82d1a9
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-07 12:32:51 +0300

    0x02-python-import_modules

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     if my_string:
         str = ''
         for i in range(len(my_string)):
-            if (my_string[i] == 'c'):
+            if (my_string[i] == 'c' or my_string[i] == 'C'):
                 continue
             else:
                 str += my_string[i]
