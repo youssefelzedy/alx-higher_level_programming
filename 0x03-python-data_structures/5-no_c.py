7775bf6574880c69d5ecc19656ffed726a2176da
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-07 12:31:02 +0300

    0x02-python-import_modules

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    if my_string:
+        str = ''
+        for i in range(len(my_string)):
+            if (my_string[i] == 'c'):
+                continue
+            else:
+                str += my_string[i]
+        return (str)
