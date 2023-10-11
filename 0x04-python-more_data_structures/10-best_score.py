ec8dda33c9b936029e01456c821bb3e1f2a49ce4
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-12 00:56:14 +0300

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     mx = -99999
-    key = -1
+    key = None
     for i in a_dictionary:
         if a_dictionary[i] > mx:
             mx = a_dictionary[i]
