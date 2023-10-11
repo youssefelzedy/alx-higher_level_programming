b12d2021afa43b844c2ed6f3e0f2260a43b995c4
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-11 20:03:34 +0300

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    lisst = []
     for i in my_list:
         if i == search:
-            i = replace
+            lisst.append(replace)
+        else:
+            lisst.append(i)
+    return lisst
