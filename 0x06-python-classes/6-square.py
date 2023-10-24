23b8b53d34322bd85e82127486dd3e62e039ee89
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-25 00:41:13 +0300

    0x06-python-classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -75,6 +75,6 @@ class Square:
             print()
             return
         else:
-            print("\n" * self.__position[1])
+            print("\n" * self.__position[1], end="")
             for i in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size, sep="")
