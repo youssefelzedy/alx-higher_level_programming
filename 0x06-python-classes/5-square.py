2028ca1b0a6257ee0ec496ddc197aa0f4b1156cc
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-25 00:02:15 +0300

    0x06-python-classes

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+
+    """ init class """
+
+    def __init__(self, size=0):
+
+        """init
+        Args:
+            size (int): size of square
+        """
+
+        self.__size = size
+
+    """ getter """
+
+    @property
+    def size(self):
+        """size getter"""
+        return (self.__size)
+
+    """ setter """
+
+    @size.setter
+    def size(self, value):
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    """ area """
+
+    def area(self):
+        """area"""
+        return (self.__size ** 2)
+
+    """ print """
+
+    def my_print(self):
+        if (self.__size == 0):
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
