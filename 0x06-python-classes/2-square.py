35b45dafd3b83aa7d064b110edd9efe19b18bcab
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-24 23:33:07 +0300

    0x06-python-classes

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """init class"""
+    def __init__(self, size=0):
+        """size init"""
+        try:
+            if size < 0 and type(size) is int: 
+               self.__size = size
+        except TypeError:
+            raise TypeError("size must be an integer")
+        except ValueError:
+            raise ValueError("size must be >= 0")
+
