5839ba91eebd1fd8709f2a678e2dfd48575d4b5d
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-30 23:24:09 +0200

    Adding File

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -77,4 +77,4 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
-            return 2 * self.__height + 2 * self.__width
+            return 2 * self.__width + 2 * self.__height
