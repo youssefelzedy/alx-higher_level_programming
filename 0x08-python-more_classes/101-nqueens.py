c0d18887025951f24c5ab1f64118d937865e2c58
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-31 20:36:47 +0200

    Adding File

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -104,6 +104,9 @@ def main():
     n = int(sys.argv[1])
     board = []
 
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
     if n < 4:
         print("N must be at least 4")
         exit(1)
