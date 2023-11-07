7bfce0dc2c7245e44011afd98ae4e7df1cab8438
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-07 19:21:20 +0200

    Adding task 8

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """Validates value"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
