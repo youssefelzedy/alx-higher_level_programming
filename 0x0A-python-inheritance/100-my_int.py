f21a0992c2a2aa9357025360b96e53d900250732
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-07 19:47:34 +0200

    Adding task 8

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+calss MyInt(int):
+    """MY ITN CLASS"""
+
+    def __eq__(self, other):
+        """Returns the opposite of the == operator"""
+        return super().__ne__(other)
+    def __ne__(self, other):
+        """Returns the opposite of the != operator"""
+        return super().__eq__(other)
