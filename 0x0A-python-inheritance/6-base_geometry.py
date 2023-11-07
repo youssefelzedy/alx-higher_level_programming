1c8cc70fa10c738b23020e10229e339fc3ad1e49
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-07 18:39:25 +0200

    Adding task 7

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
