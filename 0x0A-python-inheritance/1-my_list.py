94269f3cd09cabeba605179401bf5e465bb08081
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-07 18:23:53 +0200

    Adding task 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for is_same_class method"""
+
+
+class MyList(list):
+    """Class that inherits from list """
+
+    def print_sorted(self):
+        """Print a sorted list"""
+        print(sorted(self))
+
