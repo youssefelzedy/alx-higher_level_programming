335224d651c54b53ae03202253d7c7b019b1a6f4
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-07 18:33:05 +0200

    Adding task 3

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+
 """Module for is_same_class method"""
 
 
-def lookup(obj):
-    """Returns the list of available attributes and methods of an object
+def is_same_class(obj, a_class):
+    """Check if an object is exactly an instance of the specified class
 
     Args:
-        obj: object to look up
+        obj: object to check
+        a_class: class to check against
 
     Returns:
-        list: list of available attributes and methods of an object
+        bool: True if obj is exactly an instance of a_class, False otherwise
     """
-    return dir(obj)
+    return type(obj) == a_class
