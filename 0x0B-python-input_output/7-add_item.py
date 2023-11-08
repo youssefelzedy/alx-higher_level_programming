cc3c579e7c4987ddfa989fb0329c0f7e8127be85
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-09 00:39:20 +0200

    Adding file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Load, add, save"""
+import sys
+
+
+save = __import__('7-save_to_json_file').save_to_json_file
+load = __import__('8-load_from_json_file').load_from_json_file
+
+args = list(sys.argv[1:])
+
+try:
+    my_list = load("add_item.json")
+except Exception:
+    my_list = []
+
+my_list.extend(args)
+save(my_list, "add_item.json")
