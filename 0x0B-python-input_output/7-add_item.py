f0c25d9653b4d1bbd618a38f5af0e37e7696366a
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-09 00:40:48 +0200

    Adding file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,8 @@
 import sys
 
 
-save = __import__('7-save_to_json_file').save_to_json_file
-load = __import__('8-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
 args = list(sys.argv[1:])
 
