d988091085bae29c58c6938006af51e82cbb5ef2
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-09 00:32:49 +0200

    Adding file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create object from a JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """Create object from a JSON file"""
+    with open(filename, encoding='utf-8') as file:
+        return json.load(file)
