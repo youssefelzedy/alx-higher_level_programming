831afecac0e33f9e8712a7d34a29dff1bba5a90b
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-09 00:21:37 +0200

    Adding file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Append to a file"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, mode='a', encoding='utf-8') as file:
+        return file.write(text)
