f3077271b1eae1e3340bd202045a6805b55fab95
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-09 01:06:15 +0200

    Adding file

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Contains the class Student"""
+
+
+class Student:
+    """Representation of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary representation of a Student instance
+        or only the attrs values in the dictionary"""
+        if attrs is not None and all(isinstance(elem, str) for elem in attrs):
+            return {elem: self.__dict__[elem]
+                    for elem in attrs if elem in self.__dict__}
+        return self.__dict__
