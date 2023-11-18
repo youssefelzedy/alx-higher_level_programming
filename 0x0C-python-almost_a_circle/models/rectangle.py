b23180348488511cd0df54e3c80b5f0cd24842f7
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-18 18:33:07 +0200

    Adding file 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,28 +8,36 @@ from models.base import Base
 class Rectangle(Base):
     """Rectangle class"""
 
-    def __init__(self, width, height, x=0, y=0, id=None)
-        syper().__init__(id)
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Rectangle class
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+            x (int): x coordinate of the rectangle
+            y (int): y coordinate of the rectangle
+            id (int): id of the rectangle"""
+
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-    
+
     @property
     def width(self):
         """width getter"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """width setter"""
         self.__width = value
-    
+
     @property
     def height(self):
         """height getter"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """height setter"""
@@ -39,19 +47,18 @@ class Rectangle(Base):
     def x(self):
         """x getter"""
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         """x setter"""
         self.__x = value
-    
+
     @property
     def y(self):
         """y getter"""
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         """y setter"""
         self.__y = value
-
