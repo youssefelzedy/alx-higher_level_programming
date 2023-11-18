e9ccb8ee42669f117306d29b1ff6c1ddcb720142
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-18 22:15:04 +0200

    Adding file 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,14 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return ("[]")
         return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save to file"""
+        filename = cls.__name__ + ".json"
+        list = []
+        if list_objs is not None:
+            for obj in list_objs:
+                list.append(obj.to_dictionary())
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(list))
