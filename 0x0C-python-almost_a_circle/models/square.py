0c5254f1a337c6e4adf0511a3dcac0bade0c3102
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-18 21:39:45 +0200

    Adding file 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,3 +22,14 @@ class Square(Rectangle):
         """str method"""
         return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                   self.width))
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        self.width = value
+        self.height = value
