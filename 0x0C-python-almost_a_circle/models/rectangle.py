3fcf081801dfffa970f5d1452ef4e3bf1dd02229
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-18 21:44:04 +0200

    Adding file 13

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -122,3 +122,8 @@ class Rectangle(Base):
                     self.x = value
                 elif key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """returns the dictionary"""
+        return {"id": self.id, "width": self.width, "height": self.height,
+                "x": self.x, "y": self.y}
