f7dd046202c70217fcb6ce11c61c8caf506d924c
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-18 22:54:27 +0200

    Adding file 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,3 +35,10 @@ class Base:
                 list.append(obj.to_dictionary())
         with open(filename, "w") as f:
             f.write(cls.to_json_string(list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """from json string"""
+        if json_string is None or len(json_string) == 0:
+            return ([])
+        return (json.loads(json_string))
