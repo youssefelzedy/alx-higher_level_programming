c6fbba034fb9e120e7d8325acaa68ce196b4c452
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-18 22:08:59 +0200

    Adding file 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,3 +18,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+    
+    def to_json_string(list_dictionaries):
+        """to json string"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ("[]")
+        return (json.dumps(list_dictionaries))
