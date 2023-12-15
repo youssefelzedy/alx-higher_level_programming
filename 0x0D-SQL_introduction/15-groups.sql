9e8bbf6ac3f586c6d4ec1a7af9333ea17c3e78a5
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-12-15 02:47:12 +0200

    SQL Task

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
---lists the number of records with the same score in the table second_table 
--- of the database hbtn_0c_0 in your MySQL server (in descending order)
+-- Lists the number of records with the same score in the table second_table.
+-- Records are ordered by descending count.
 SELECT `score`, COUNT(*) AS `number`
 FROM `second_table`
 GROUP BY `score`
