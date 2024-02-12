d4847f3e855c6568239c58913a136baaed525ab0
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-02-12 22:44:51 +0200

    final work

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const len = parseInt(process.argv[2]);
+if (!len) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < len; i++) {
+    console.log('C is fun');
+  }
+}
