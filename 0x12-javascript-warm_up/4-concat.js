d5e3880d0c8e26726db3f7221641d14b4f1af7a1
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-02-12 21:45:51 +0200

    final work

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length === 2)
-{
-    console.log('undefined is undefined');
-}
-else if (process.argv.length === 3)
-{
-    console.log(`${process.argv[2]} is undefined`);
-}
-else if (process.argv.length === 4)
-{
-    console.log(`${process.argv[2]} is ${process.argv[3]}`);
+if (process.argv.length === 2) {
+  console.log('undefined is undefined');
+} else if (process.argv.length === 3) {
+  console.log(`${process.argv[2]} is undefined`);
+} else if (process.argv.length === 4) {
+  console.log(`${process.argv[2]} is ${process.argv[3]}`);
 }
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-if (process.argv.length === 2)
-{
-    console.log('Not a number');
-}
-else
-{
-    console.log(`My number: ${parseInt(process.argv[2])}`);
+const num = parseInt(process.argv[2]);
+if (!num) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${num}`);
 }
