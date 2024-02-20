ca32342e1a00f8c18abf649b4c28da36d1675c05
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-02-21 00:57:59 +0200

    final work

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     name = sys.argv[4]
-    cur.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name`\
+    cur.execute("SELECT `cities`.`name`\
                 FROM `cities`\
                 JOIN `states` ON `cities`.`state_id` = `states`.`id`\
                 WHERE `states`.`name` = %s\
