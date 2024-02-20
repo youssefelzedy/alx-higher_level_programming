39a7a7521852b4760e0bfb2e1db92bdd98373908
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-02-21 00:05:41 +0200

    final work

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,9 +11,9 @@ Usage: ./2-my_filter_states.py <mysql username> <mysql password>
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
     name = sys.argv[4]
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,12 +11,13 @@ Usage: ./3-my_safe_filter_states.py <mysql username> <mysql password>
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     name = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name = %s", (name, ))
+    cur.execute("SELECT * FROM states WHERE name = %s",
+                (name, ))
     for row in cur.fetchall():
         print(row)
     cur.close()
