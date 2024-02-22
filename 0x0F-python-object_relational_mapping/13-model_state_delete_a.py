10ddfcff75a0c4357bedcbf4e08fe14f0a5af652
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-02-22 19:09:25 +0200

    final work

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+11-model_state_insert.py: script that adds the State object "Louisiana" to the
+database hbtn_0e_6_usa
+Usage: ./11-model_state_insert.py <mysql username> <mysql password>
+<database name>
+'''
+
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states_with_a = session.query(State).filter(State.name.like('%a%')).all()
+    if states_with_a:
+        for state in states_with_a:
+            session.delete(state)
+            
+        session.commit()
+
+    session.close()
