52b89b97be75952a2b9b089cc833ec13e2b05f6f
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-02-22 20:31:05 +0200

    final work

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,7 +3,7 @@
 base srate model class for the project
 '''
 
-from sqlalchemy import create_engine, ForeignKey,\
+from sqlalchemy import create_engine, ForeignKey, \
         Column, String, Integer, VARCHAR
 from model_state import Base, State
 from sqlalchemy.orm import declarative_base
@@ -16,7 +16,8 @@ class City(Base):
     cities class
     '''
     __tablename__ = 'cities'
+
     id = Column('id', Integer, primary_key=True)
     name = Column('name', VARCHAR(128), nullable=False)
-    state_id = Column('state_id',   Integer,  
+    state_id = Column('state_id',   Integer,
                       ForeignKey('State.id'), nullable=False)
